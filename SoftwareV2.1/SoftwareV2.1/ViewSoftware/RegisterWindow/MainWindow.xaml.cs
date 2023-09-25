2857a255f474b414eb5effaf43b4a7da26709afd
FaridJan <126804007+1321fFard@users.noreply.github.com>
2023-09-25 11:29:43 +0300

    Add files via upload

---
+++ SoftwareV2.1/SoftwareV2.1/ViewSoftware/RegisterWindow/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using SoftwareV2._1.ViewSoftware.RegisterWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoftwareV2._1
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow registerAccount = new();
+            this.Close();
+            registerAccount.Show();
+        }
+
+        private void login_to_Account_Click(object sender, RoutedEventArgs e)
+        {
+            if (login.Text.Length > 0)
+            {
+                if (password.Password.Length > 0)
+                {
+                    MessageBox.Show("Успешный вход");
+                }
+                else MessageBox.Show("Введите пароль");
+            }
+            else MessageBox.Show("Введите логин");
+        }
+    }
+}
---
+++ SoftwareV2.1/SoftwareV2.1/ViewSoftware/RegisterWindow/RegisterWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SoftwareV2._1.ViewSoftware.RegisterWindow
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void New_Account_Click(object sender, RoutedEventArgs e)
+        {
+            if (NewLogin.Text.Length > 0)
+            {
+                if (NewPassword.Password.Length > 0)
+                {
+                    if (RepeatPassword.Password.Length > 0)
+                    {
+                        if (NewPassword.Password == RepeatPassword.Password)
+                        {
+                            MessageBox.Show("Успешный вход");
+                        }
+                        else MessageBox.Show("Пароли не совпадают");
+                    }
+                    else MessageBox.Show("Вы забыли ввести повторный пароль");
+                }
+                else MessageBox.Show("Вы забыли придумать пароль");
+            }
+            else MessageBox.Show("Вы забыли придумать логин");
+        }
+
+        private void Login_to_Account_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new();
+            this.Close();
+            mainWindow.Show();
+        }
+    }
+}
